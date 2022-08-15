6ef88e79ca680eb057d18583680b1fc2130ff087
Guigp <guilherme.gp85@hotmail.com>
2022-08-15 14:43:58 -0300

    disable  oauth

--- server/src/app.js
+++ server/src/app.js
@@ -17,53 +17,53 @@ app.use(
 );
 
 ////////////////AUTENTICAÇÂO POR TOKEN///////////////////
-const config = {
-  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
-  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
-};
+// const config = {
+//   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+//   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+//   COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+//   COOKIE_KEY_2: process.env.COOKIE_KEY_2,
+// };
 
-const AUTH_OPTIONS = {
-  callbackURL: "/v1/auth/google/callback",
-  clientID: config.GOOGLE_CLIENT_ID,
-  clientSecret: config.GOOGLE_CLIENT_SECRET,
-};
+// const AUTH_OPTIONS = {
+//   callbackURL: "/v1/auth/google/callback",
+//   clientID: config.GOOGLE_CLIENT_ID,
+//   clientSecret: config.GOOGLE_CLIENT_SECRET,
+// };
 
 //função que no fim recebe o token do google
-function verifyCallback(accessToken, refreshToken, profile, done) {
-  console.log(profile, accessToken);
-  done(null, profile);
-}
+// function verifyCallback(accessToken, refreshToken, profile, done) {
+//   console.log(profile, accessToken);
+//   done(null, profile);
+// }
 
-passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+//  passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
 ////////////////FIM AUTENTICAÇÂO POR TOKEN///////////////////
 
 //////////////// AUTENTICAÇÂO POR COOKIE SESSION///////////////////
 
-//salva a session no cookie para enviar pro client
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
+// //salva a session no cookie para enviar pro client
+// passport.serializeUser((user, done) => {
+//   done(null, user.id);
+// });
 
-//a cada req le o cookie e armazena em req
-passport.deserializeUser((obj, done) => {
-  done(null, obj);
-});
+// //a cada req le o cookie e armazena em req
+// passport.deserializeUser((obj, done) => {
+//   done(null, obj);
+// });
 
-app.use(
-  cookieSession({
-    name: "session",
-    maxAge: 24 * 60 * 60 * 1000,
-    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
-  })
-);
+// app.use(
+//   cookieSession({
+//     name: "session",
+//     maxAge: 24 * 60 * 60 * 1000,
+//     keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+//   })
+// );
 
 ////////////////FIM AUTENTICAÇÂO POR COOKIE SESSION///////////////////
 
-app.use(passport.initialize());
-app.use(passport.session()); //passport entende o cookie-session
+//app.use(passport.initialize());
+//app.use(passport.session()); //passport entende o cookie-session
 //app.use(morgan("combined"));
 
 app.use(express.json());
--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -7,43 +7,43 @@ const passport = require("passport");
 
 const api = express.Router();
 
-function checkLoggedIn(req, res, next) {
-  console.log(`Current user is:${req.user}`);
-  const isLogged = req.isAuthenticaded(); //vem do passport
-  if (!isLogged) {
-    return res.status(401).json({
-      error: "You must be log in@!",
-    });
-  }
-  next();
-}
+// function checkLoggedIn(req, res, next) {
+//   console.log(`Current user is:${req.user}`);
+//   const isLogged = req.isAuthenticaded(); //vem do passport
+//   if (!isLogged) {
+//     return res.status(401).json({
+//       error: "You must be log in@!",
+//     });
+//   }
+//   next();
+// }
 
-api.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["email"],
-  })
-);
-//aqui o passport usa o codigo que o google enviou para mandar outra req pro /token do google e assim recebe de volta o token
-api.get(
-  "/auth/google/callback",
-  passport.authenticate("google", {
-    failureRedirect: "/failure",
-    successRedirect: "/",
-    session: true, //para usar cookies
-  }),
-  (req, res) => {
-    console.log("Google called us back!");
-  }
-);
+// api.get(
+//   "/auth/google",
+//   passport.authenticate("google", {
+//     scope: ["email"],
+//   })
+// );
+// //aqui o passport usa o codigo que o google enviou para mandar outra req pro /token do google e assim recebe de volta o token
+// api.get(
+//   "/auth/google/callback",
+//   passport.authenticate("google", {
+//     failureRedirect: "/failure",
+//     successRedirect: "/",
+//     session: true, //para usar cookies
+//   }),
+//   (req, res) => {
+//     console.log("Google called us back!");
+//   }
+// );
 
-api.get("/failure", (req, res) => {
-  return res.send("Falied to log in!");
-});
-api.get("/auth/logout", (req, res) => {
-  req.logOut(); //vem do passport
-  return res.redirect("/");
-});
+// api.get("/failure", (req, res) => {
+//   return res.send("Falied to log in!");
+// });
+// api.get("/auth/logout", (req, res) => {
+//   req.logOut(); //vem do passport
+//   return res.redirect("/");
+// });
 api.use("/planets", planetsRouter);
 api.use("/launches", launchesRouter);
 
